3c8f2caa077f83d9f909781f56cdca80f151a9c6
Anthonyo3477 <anthonyoverdugo55@gmail.com>
2025-08-25 15:45:23 -0400

    arreglos de nombres de algunas vistas

--- db/controllers/indexController.js
+++ db/controllers/indexController.js
@@ -1,4 +1,4 @@
 exports.mostrarHome = (req, res) => {
     const Home = []; // o tus datos reales
-    res.render('home', { title: 'Colegio San Francisco Carrasco - Inicio 222', Home });
+    res.render('home', { title: 'Colegio Manuel Francisco Correa - Inicio 222', Home });
 };
